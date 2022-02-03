2efa3600e840cb92f6a8ab36d6d3f8821d1d48e8
Clara Zhang <85580892+zhihuizhang0625@users.noreply.github.com>
2022-02-03 00:11:30 -0500

    Update Tuple.java

--- Tuple.java
+++ Tuple.java
@@ -9,40 +9,40 @@ public class Tuple<L, R> {
     private L left;
     private R right;
 
-	/**
+    /**
 	 * @param l
 	 * @param r
 	 */
-public Tuple(L l, R r) {
-    this.setLeft(l);
-	this.setRight(r);
-}
+    public Tuple(L l, R r) {
+        this.setLeft(l);
+	    this.setRight(r);
+    }
 
 	/**
 	 * @return the right
 	 */
-public R getRight() {
-  return right;
+    public R getRight() {
+       return right;
 	}
 
 	/**
 	 * @param right the right to set
 	 */
-public void setRight(R right) {
-    this.right = right;
+    public void setRight(R right) {
+        this.right = right;
 }
 
 	/**
 	 * @return the left
 	 */
-public L getLeft() {
+    public L getLeft() {
 		return left;
 	}
 
 	/**
 	 * @param left the left to set
 	 */
-public void setLeft(L left) {
+    public void setLeft(L left) {
 		this.left = left;
 	}
 
