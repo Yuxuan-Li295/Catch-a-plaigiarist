ae8b617697ddd502dbf5cd8af10273fae1188af9
Clara Zhang <85580892+zhihuizhang0625@users.noreply.github.com>
2022-02-02 22:28:21 -0500

    Update DocumentsProcessorTest.java

--- DocumentsProcessorTest.java
+++ DocumentsProcessorTest.java
@@ -12,72 +12,79 @@ public class DocumentsProcessorTest {
 	List<String> testlist = new ArrayList<>();
 	List<Tuple<String, Integer>> tuplelistTest = new ArrayList<>();
 	TreeSet<Similarities> treeset2 = new TreeSet<>();
-	
+	String dir = "/autograder/submission/test files";
+	String file1 = "/autograder/submission/file1.txt";
+	String file2 = "/autograder/submission/file2.txt";
+	String file3 = "/autograder/submission/file3.txt";
+	String file4 = "/autograder/submission/file4.txt";
+	String file5 = "/autograder/submission/file5.txt";
+	String nwordFilePath = "/autograder/submission/nwordFilePath.txt";
+	String sequenceFile = "/autograder/submission/sequenceFile.txt";
 	@org.junit.Test
 	public void ProcessDocuments() {
-			testlist.add("thisis");
-			testlist.add("isa");
-			testlist.add("atest");
-			testlist.add("testdocument");
-			testmap = processor.processDocuments("/autograder/submission/test files", 2);
-			assertEquals(testlist, testmap.get("/autograder/submission/file1.txt"));
-		}
-	
+		testlist.add("thisis");
+		testlist.add("isa");
+		testlist.add("atest");
+		testlist.add("testdocument");
+		testmap = processor.processDocuments(dir, 2);
+		assertEquals(testlist, testmap.get(file1));
+	}
+
 	@org.junit.Test
 	public void teststoreNWordSequences1() {
-		testmap = processor.processDocuments("/autograder/submission/test files", 4);
-		tuplelistTest = processor.storeNWordSequences(testmap, "/autograder/submission/nwordFilePath.txt");
+		testmap = processor.processDocuments(dir, 4);
+		tuplelistTest = processor.storeNWordSequences(testmap, nwordFilePath);
 		int size = 0;
-		for(Tuple<String, Integer> tuple: tuplelistTest) {
-			if(tuple.getLeft().equals("/autograder/submission/file1.txt")) {
-			 size = tuple.getRight();
+		for (Tuple<String, Integer> tuple : tuplelistTest) {
+			if (tuple.getLeft().equals(file1)) {
+				size = tuple.getRight();
 			}
 		}
 		assertEquals(28, size);
 	}
-	
+
 	@org.junit.Test
 	public void computeSimilarities() {
 		testmap = processor.processDocuments("test files 2", 3);
-		tuplelistTest = processor.storeNWordSequences(testmap, "/autograder/submission/nwordFilePath.txt");	
-		treeset2 = processor.computeSimilarities("/autograder/submission/nwordFilePath.txt",tuplelistTest);
-		for(Similarities sb : treeset2)
-		{
-			
-			if(sb.getFile1().equals("/autograder/submission/file5.txt")&& sb.getFile2().equals("/autograder/submission/file4.txt")) {
+		tuplelistTest = processor.storeNWordSequences(testmap, nwordFilePath);
+		treeset2 = processor.computeSimilarities(nwordFilePath, tuplelistTest);
+		for (Similarities sb : treeset2) {
+
+			if (sb.getFile1().equals(file5)
+					&& sb.getFile2().equals(file4)) {
 				assertEquals(3, sb.getCount());
-				
+
 			}
 		}
 	}
+
 	@org.junit.Test
 	public void printSimilarities() {
-		Similarities s1 = new Similarities("/autograder/submission/test files/file1.txt", "/autograder/submission/test files/file2.txt");
+		Similarities s1 = new Similarities(file1,file2);
 		s1.setCount(1);
 		treeset2.add(s1);
-		Similarities s2 = new Similarities("/autograder/submission/test files 2/file3.txt", "/autograder/submission/test files/file2.txt");
+		Similarities s2 = new Similarities(file3,file2);
 		s1.setCount(3);
 		treeset2.add(s2);
 		processor.printSimilarities(treeset2, 1);
-		int n = treeset2.size(); 
-        List<Similarities> list = new ArrayList<>();
-        Similarities arr[] = new Similarities[2];
-        int i = 0;
-        for (Similarities s : treeset2) 
-            arr[i++] = s; 
-        assertEquals(s2,arr[0]);
-    
+		Similarities arr[] = new Similarities[2];
+		int i = 0;
+		for (Similarities s : treeset2)
+			arr[i++] = s;
+		assertEquals(s2, arr[0]);
+
 	}
+
 	@org.junit.Test
 	public void processAndStore() {
-		tuplelistTest = processor.processAndStore("/autograder/submission/test files", "/autograder/submission/sequenceFile.txt", 4);
+		tuplelistTest = processor.processAndStore(dir,sequenceFile, 4);
 		int size = 0;
-		for(Tuple<String, Integer> tuple: tuplelistTest) {
-			if(tuple.getLeft().equals("/autograder/submission/test files/file1.txt")) {
-			 size = tuple.getRight();
+		for (Tuple<String, Integer> tuple : tuplelistTest) {
+			if (tuple.getLeft().equals(file1)) {
+				size = tuple.getRight();
 			}
 		}
 		assertEquals(28, size);
 	}
-	
+
 }
