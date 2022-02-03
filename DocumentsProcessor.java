792a97b8a6d7a7ff1262f518faabe7c57bf4f9b9
Clara Zhang <85580892+zhihuizhang0625@users.noreply.github.com>
2022-02-02 22:29:51 -0500

    Update DocumentsProcessor.java

--- DocumentsProcessor.java
+++ DocumentsProcessor.java
@@ -27,7 +27,8 @@ public class DocumentsProcessor implements IDocumentsProcessor {
 
 			for (int i = 0; i < filesinfolder.length; i++) {
 				List<String> processList = new ArrayList<>();
-				if (filesinfolder[i].isFile() && filesinfolder[i].getName().substring(filesinfolder[i].getName().length() - 4).equals(".txt")) {
+				if (filesinfolder[i].isFile() && filesinfolder[i].getName()
+						.substring(filesinfolder[i].getName().length() - 4).equals(".txt")) {
 					BufferedReader reader = new BufferedReader(new FileReader(filesinfolder[i]));
 					DocumentIterator Iterator = new DocumentIterator(reader, n);
 
@@ -40,8 +41,8 @@ public class DocumentsProcessor implements IDocumentsProcessor {
 						reader.close();
 					} catch (IOException e) {
 						e.printStackTrace();
-					} 
-				} 
+					}
+				}
 			}
 
 		} catch (FileNotFoundException e) {
@@ -59,7 +60,6 @@ public class DocumentsProcessor implements IDocumentsProcessor {
 			// RAF
 			BufferedWriter writer = new BufferedWriter(new FileWriter(nwordFilePath));
 			StringBuilder File = new StringBuilder("");
-			
 
 			for (Map.Entry<String, List<String>> entry : docs.entrySet()) {
 				for (String ListString : entry.getValue()) {
@@ -70,14 +70,13 @@ public class DocumentsProcessor implements IDocumentsProcessor {
 				tuplelist.add(tuple);
 				writer.write(File.toString());
 				File.setLength(0);
-				
+
 			}
-			writer.close();			
-		}
-		catch (IOException e) {
+			writer.close();
+		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		
+
 		return tuplelist;
 	}
 
@@ -93,6 +92,9 @@ public class DocumentsProcessor implements IDocumentsProcessor {
 			@Override
 			public int compare(Similarities o1, Similarities o2) {
 				if (o1.getFile1() == o2.getFile1() && o1.getFile2() == o2.getFile2()) {
+					return 0;
+				}
+				if (o1.getFile1() == o2.getFile2() && o1.getFile2() == o2.getFile1()) {
 					return 0;
 				} else {
 					return -1;
@@ -114,26 +116,26 @@ public class DocumentsProcessor implements IDocumentsProcessor {
 						current = raf.readByte();
 
 						// end of word
-						if ((char)current ==' ') {
+						if ((char) current == ' ') {
 
 							// same word
 							if (wordmap.containsKey(sb.toString())) {
 								// same word, different file
 								if (!wordmap.get(sb.toString()).contains(filename)) {
-								
-										for (int z = 0; z < wordmap.get(sb.toString()).size(); z++) {
 
-											String prevfile = wordmap.get(sb.toString()).get(z);
-											Similarities similarity = new Similarities(prevfile, filename);
-											if (!treeset.contains(similarity)) {
+									for (int z = 0; z < wordmap.get(sb.toString()).size(); z++) {
 
-												treeset.add(similarity);
-												similarity.setCount(1);
-											} else {
-												Similarities s = (Similarities) ((TreeSet<Similarities>) treeset)
-														.ceiling(similarity);
-												s.setCount((int) s.getCount() + 1);
-											}	
+										String prevfile = wordmap.get(sb.toString()).get(z);
+										Similarities similarity = new Similarities(prevfile, filename);
+										if (!treeset.contains(similarity)) {
+
+											treeset.add(similarity);
+											similarity.setCount(1);
+										} else {
+											Similarities s = (Similarities) ((TreeSet<Similarities>) treeset)
+													.ceiling(similarity);
+											s.setCount((int) s.getCount() + 1);
+										}
 									}
 									wordmap.get(sb.toString()).add(filename);
 								}
@@ -151,7 +153,7 @@ public class DocumentsProcessor implements IDocumentsProcessor {
 						// adding characters
 						else {
 //							word += (char) current;
-							sb.append((char)current);
+							sb.append((char) current);
 						}
 
 					}
@@ -163,7 +165,6 @@ public class DocumentsProcessor implements IDocumentsProcessor {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
-
 
 		}
 
@@ -179,71 +180,34 @@ public class DocumentsProcessor implements IDocumentsProcessor {
 	public void printSimilarities(TreeSet<Similarities> sims, int threshold) {
 		// TODO Auto-generated method stub
 
-		
-			Comparator<Similarities> comp = new Comparator<Similarities>() {
-				@Override
-				public int compare(Similarities o1, Similarities o2) {
-					if(o2.getCount()>o1.getCount()) {
-						return 5;
-					}
-					if(o2.getCount()<o1.getCount()) {
-						return -5;
-					}
-					if(o2.getFile1().compareTo(o1.getFile1()) >0 &&
-							o2.getFile1().compareTo(o1.getFile1()) >0) {
-						return 4;
-					}
-					if(o2.getFile1().compareTo(o1.getFile1()) >0 &&
-							o2.getFile1().compareTo(o1.getFile1()) == 0) {
-						return 3;
-					}
-					if(o2.getFile1().compareTo(o1.getFile1()) >0 &&
-							o2.getFile1().compareTo(o1.getFile1()) <0) {
-						return 2;
-					}
-					if(o2.getFile1().compareTo(o1.getFile1()) ==0 &&
-							o2.getFile1().compareTo(o1.getFile1()) >0) {
-						return 1;
-					}
-					
-					if(o2.getFile1().compareTo(o1.getFile1()) ==0 &&
-							o2.getFile1().compareTo(o1.getFile1()) <0) {
-						return -1;
-					}if(o2.getFile1().compareTo(o1.getFile1()) <0 &&
-							o2.getFile1().compareTo(o1.getFile1()) >0) {
-						return -2;
-					}
-					if(o2.getFile1().compareTo(o1.getFile1()) <0 &&
-							o2.getFile1().compareTo(o1.getFile1()) == 0) {
-						return -3;
-					}
-					if(o2.getFile1().compareTo(o1.getFile1()) <0 &&
-							o2.getFile1().compareTo(o1.getFile1()) <0) {
-						return -4;
-					}
-					
-						return 0;
-
+		Comparator<Similarities> comp = new Comparator<Similarities>() {
+			@Override
+			public int compare(Similarities o1, Similarities o2) {
+				if (o2.getCount() == o1.getCount()) {
+					return o1.compareTo(o2);
 				}
-			};
-			
-			TreeSet<Similarities> finalSet = new TreeSet<>(comp);
-			for(Similarities sb : sims)
-			{
-				finalSet.add(sb);
+
+				if (o2.getCount() > o1.getCount()) {
+					return 1;
+				} else {
+					return -1;
+				}
 			}
-	        finalSet.addAll(sims);
-	        for(Similarities s: finalSet) {
+		};
+
+		TreeSet<Similarities> finalSet = new TreeSet<>(comp);
+		for (Similarities sb : sims) {
+			finalSet.add(sb);
+		}
+		finalSet.addAll(sims);
+		for (Similarities s : finalSet) {
 			if (s.getCount() > threshold) {
-				System.out.println(s.getFile1() + " " + s.getFile2() + " " + s.getCount());
+				System.out.println("Method 4: " + s.getFile1() + " " + s.getFile2() + " " + s.getCount());
 
 			}
-			}
-			
-
 		}
 
-	
+	}
 
 	@Override
 	public List<Tuple<String, Integer>> processAndStore(String directoryPath, String sequenceFile, int n) {
@@ -253,23 +217,24 @@ public class DocumentsProcessor implements IDocumentsProcessor {
 			File folder2 = new File(directoryPath);
 			File[] filesinfolder2 = folder2.listFiles();
 			BufferedWriter writer2 = new BufferedWriter(new FileWriter(sequenceFile));
-			
+
 			for (int i = 0; i < filesinfolder2.length; i++) {
-				if (filesinfolder2[i].isFile() && filesinfolder2[i].getName().substring(filesinfolder2[i].getName().length() - 4).equals(".txt")) {
+				if (filesinfolder2[i].isFile() && filesinfolder2[i].getName()
+						.substring(filesinfolder2[i].getName().length() - 4).equals(".txt")) {
 					BufferedReader reader5 = new BufferedReader(new FileReader(filesinfolder2[i]), n);
 					DocumentIterator Iterator = new DocumentIterator(reader5, n);
 					while (Iterator.hasNext()) {
 						File.append(Iterator.next() + " ");
 					}
 
-					reader5.close(); 
+					reader5.close();
 				}
 				Tuple<String, Integer> tuple = new Tuple<>(filesinfolder2[i].getName(), File.toString().length());
 				tuplelist2.add(tuple);
 				writer2.write(File.toString());
 				File.setLength(0);
 			}
-			writer2.close(); 
+			writer2.close();
 		}
 
 		catch (IOException e) {
