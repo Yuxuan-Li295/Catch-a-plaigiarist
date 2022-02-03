53e6eb89eb119b1fef572024f603bbe24d0514ca
Clara Zhang <85580892+zhihuizhang0625@users.noreply.github.com>
2022-02-03 01:01:12 -0500

    Update DocumentsProcessor.java

--- DocumentsProcessor.java
+++ DocumentsProcessor.java
@@ -16,168 +16,167 @@ import java.util.TreeSet;
 
 public class DocumentsProcessor implements IDocumentsProcessor {
 
-	@Override
+    @Override
 	public Map<String, List<String>> processDocuments(String directoryPath, int n) {
 		// TODO Auto-generated method stub
-		Map<String, List<String>> processMap = new HashMap<>();
+        Map<String, List<String>> processMap = new HashMap<>();
+ 
+        try {
+            File folder = new File(directoryPath);
+            File[] filesinfolder = folder.listFiles();
 
-		try {
-			File folder = new File(directoryPath);
-			File[] filesinfolder = folder.listFiles();
+            for (int i = 0; i < filesinfolder.length; i++) {
+                List<String> processList = new ArrayList<>();
+                if (filesinfolder[i].isFile() && filesinfolder[i].getName()
+.substring(filesinfolder[i].getName().length() - 4).equals(".txt")) {
+                    BufferedReader reader = new BufferedReader(new FileReader(filesinfolder[i]));
+                    DocumentIterator Iterator = new DocumentIterator(reader, n);
 
-			for (int i = 0; i < filesinfolder.length; i++) {
-				List<String> processList = new ArrayList<>();
-				if (filesinfolder[i].isFile() && filesinfolder[i].getName()
-						.substring(filesinfolder[i].getName().length() - 4).equals(".txt")) {
-					BufferedReader reader = new BufferedReader(new FileReader(filesinfolder[i]));
-					DocumentIterator Iterator = new DocumentIterator(reader, n);
+                    while (Iterator.hasNext()) {
+                        processList.add(Iterator.next());
+                    }
+                    processMap.put(filesinfolder[i].getName(), processList);
 
-					while (Iterator.hasNext()) {
-						processList.add(Iterator.next());
-					}
-					processMap.put(filesinfolder[i].getName(), processList);
+                    try {
+                        reader.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
 
-					try {
-						reader.close();
-					} catch (IOException e) {
-						e.printStackTrace();
-					}
-				}
-			}
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
 
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		}
-
-		return processMap;
+        return processMap;
 	}
 
 	@Override
 	public List<Tuple<String, Integer>> storeNWordSequences(Map<String, List<String>> docs, String nwordFilePath) {
-		Tuple<String, Integer> tuple;
-		List<Tuple<String, Integer>> tuplelist = new ArrayList<>();
-		try {
+        Tuple<String, Integer> tuple;
+        List<Tuple<String, Integer>> tuplelist = new ArrayList<>();
+        try {
 			// RAF
-			BufferedWriter writer = new BufferedWriter(new FileWriter(nwordFilePath));
-			StringBuilder File = new StringBuilder("");
+            BufferedWriter writer = new BufferedWriter(new FileWriter(nwordFilePath));
+            StringBuilder File = new StringBuilder("");
 
-			for (Map.Entry<String, List<String>> entry : docs.entrySet()) {
-				for (String ListString : entry.getValue()) {
-					File.append(ListString + " ");
-				}
+            for (Map.Entry<String, List<String>> entry : docs.entrySet()) {
+                for (String ListString : entry.getValue()) {
+                    File.append(ListString + " ");
+            }
+                int filelength = File.toString().length();
+				tuple = new Tuple<String, Integer>(entry.getKey(), filelength);
+                tuplelist.add(tuple);
+                writer.write(File.toString());
+                File.setLength(0);
 
-				tuple = new Tuple<String, Integer>(entry.getKey(), File.toString().length());
-				tuplelist.add(tuple);
-				writer.write(File.toString());
-				File.setLength(0);
+            }
+            writer.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
-			}
-			writer.close();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-
-		return tuplelist;
-	}
+        return tuplelist;
+    }
 
 	@Override
 	public TreeSet<Similarities> computeSimilarities(String nwordFilePath, List<Tuple<String, Integer>> fileindex) {
 
-		int current = 0;
-		RandomAccessFile raf;
+        int current = 0;
+        RandomAccessFile raf;
 //		String word = "";
-		StringBuilder sb = new StringBuilder("");
-		Map<String, List<String>> wordmap = new HashMap<>();
-		Comparator<Similarities> comp = new Comparator<Similarities>() {
+        StringBuilder sb = new StringBuilder("");
+        Map<String, List<String>> wordmap = new HashMap<>();
+        Comparator<Similarities> comp = new Comparator<Similarities>() {
 			@Override
 			public int compare(Similarities o1, Similarities o2) {
-				if (o1.getFile1() == o2.getFile1() && o1.getFile2() == o2.getFile2()) {
-					return 0;
-				}
-				if (o1.getFile1() == o2.getFile2() && o1.getFile2() == o2.getFile1()) {
-					return 0;
-				} else {
-					return -1;
-				}
+                        if (o1.getFile1() == o2.getFile1() && o1.getFile2() == o2.getFile2()) {
+                            return 0;
+                        }
+                        if (o1.getFile1() == o2.getFile2() && o1.getFile2() == o2.getFile1()) {
+                            return 0;
+                        } else {
+                            return -1;
+                        }
 			}
 		};
-		TreeSet<Similarities> treeset = new TreeSet<>(comp);
-		try {
-			raf = new RandomAccessFile(nwordFilePath, "r");
-			try {
+        TreeSet<Similarities> treeset = new TreeSet<>(comp);
+        try {
+            raf = new RandomAccessFile(nwordFilePath, "r");
+            try {
 				// loop through nwordFilePath
-				for (int j = 0; j < fileindex.size(); j++) {
-					int index = fileindex.get(j).getRight();
-					String filename = fileindex.get(j).getLeft();
+                for (int j = 0; j < fileindex.size(); j++) {
+                    int index = fileindex.get(j).getRight();
+                    String filename = fileindex.get(j).getLeft();
 
 					// loop through each file
-					for (int i = 0; i < index; i++) {
+                for (int i = 0; i < index; i++) {
 
-						current = raf.readByte();
+                    current = raf.readByte();
 
 						// end of word
-						if ((char) current == ' ') {
+                        if ((char) current == ' ') {
 
 							// same word
-							if (wordmap.containsKey(sb.toString())) {
+                            if (wordmap.containsKey(sb.toString())) {
 								// same word, different file
-								if (!wordmap.get(sb.toString()).contains(filename)) {
+                                if (!wordmap.get(sb.toString()).contains(filename)) {
 
-									for (int z = 0; z < wordmap.get(sb.toString()).size(); z++) {
+                                    for (int z = 0; z < wordmap.get(sb.toString()).size(); z++) {
 
 										String prevfile = wordmap.get(sb.toString()).get(z);
-										Similarities similarity = new Similarities(prevfile, filename);
+                                        Similarities similarity = new Similarities(prevfile, filename);
 										if (!treeset.contains(similarity)) {
 
-											treeset.add(similarity);
-											similarity.setCount(1);
-										} else {
-											Similarities s = (Similarities) ((TreeSet<Similarities>) treeset)
-													.ceiling(similarity);
-											s.setCount((int) s.getCount() + 1);
-										}
-									}
-									wordmap.get(sb.toString()).add(filename);
-								}
-								sb.setLength(0);
-							}
+                                            treeset.add(similarity);
+                                            similarity.setCount(1);
+                                        } else {
+	                                        Similarities s = (Similarities) ((TreeSet<Similarities>) treeset).ceiling(similarity);
+                                        s.setCount((int) s.getCount() + 1);
+                                        }
+                                    }
+                                    wordmap.get(sb.toString()).add(filename);
+                                }
+                                sb.setLength(0);
+                            }
 
 							// different word
-							else {
-								List<String> filelist = new ArrayList<>();
-								filelist.add(filename);
-								wordmap.put(sb.toString(), filelist);
-								sb.setLength(0);
-							}
-						}
+                            else {
+                                List<String> filelist = new ArrayList<>();
+                                filelist.add(filename);
+                                wordmap.put(sb.toString(), filelist);
+                                sb.setLength(0);
+                            }
+                        }
 						// adding characters
-						else {
+                        else {
 //							word += (char) current;
-							sb.append((char) current);
-						}
+                            sb.append((char) current);
+                        }
 
-					}
-				}
+                    }
+                }
 
-			}
+            }
 
-			catch (IOException e) {
+            catch (IOException e) {
 				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
+                e.printStackTrace();
+            }
 
-		}
+        }
 
-		catch (FileNotFoundException e) {
+        catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+            e.printStackTrace();
+        }
 
-		return treeset;
-	}
+        return treeset;
+     }
 
 	@Override
-	public void printSimilarities(TreeSet<Similarities> sims, int threshold) {
+     public void printSimilarities(TreeSet<Similarities> sims, int threshold) {
 		// TODO Auto-generated method stub
 
 		Comparator<Similarities> comp = new Comparator<Similarities>() {
