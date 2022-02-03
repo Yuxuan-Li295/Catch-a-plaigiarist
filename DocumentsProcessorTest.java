7b5be0e9a26ac1c3a1f21df65e4e3788c54d7a85
Clara Zhang <85580892+zhihuizhang0625@users.noreply.github.com>
2022-02-03 08:53:25 -0500

    Update DocumentsProcessorTest.java

--- DocumentsProcessorTest.java
+++ DocumentsProcessorTest.java
@@ -12,27 +12,29 @@ public class DocumentsProcessorTest {
 	List<String> testlist = new ArrayList<>();
 	List<Tuple<String, Integer>> tuplelistTest = new ArrayList<>();
 	TreeSet<Similarities> treeset2 = new TreeSet<>();
-	String dir = "/autograder/submission/test files";
+	String dir1 = "/autograder/submission/test_files";
+	String dir2 = "/autograder/submission/test_files_2";
 	String file1 = "/autograder/submission/file1.txt";
-	String file2 = "/autograder/submission/file2.txt";
+	String file2 = "/autograder/submission/file2.txt"; 
 	String file3 = "/autograder/submission/file3.txt";
 	String file4 = "/autograder/submission/file4.txt";
 	String file5 = "/autograder/submission/file5.txt";
 	String nwordFilePath = "nwordFilePath.txt";
 	String sequenceFile = "sequenceFile.txt";
+
 	@org.junit.Test
 	public void ProcessDocuments() {
 		testlist.add("thisis");
 		testlist.add("isa");
 		testlist.add("atest");
 		testlist.add("testdocument");
-		testmap = processor.processDocuments(dir, 2);
+		testmap = processor.processDocuments(dir1, 2);
 		assertEquals(testlist, testmap.get(file1));
 	}
 
 	@org.junit.Test
 	public void teststoreNWordSequences1() {
-		testmap = processor.processDocuments(dir, 4);
+		testmap = processor.processDocuments(dir1, 4);
 		tuplelistTest = processor.storeNWordSequences(testmap, nwordFilePath);
 		int size = 0;
 		for (Tuple<String, Integer> tuple : tuplelistTest) {
@@ -45,13 +47,12 @@ public class DocumentsProcessorTest {
 
 	@org.junit.Test
 	public void computeSimilarities() {
-		testmap = processor.processDocuments("test files 2", 3);
+		testmap = processor.processDocuments(dir2, 3);
 		tuplelistTest = processor.storeNWordSequences(testmap, nwordFilePath);
 		treeset2 = processor.computeSimilarities(nwordFilePath, tuplelistTest);
 		for (Similarities sb : treeset2) {
 
-			if (sb.getFile1().equals(file5)
-					&& sb.getFile2().equals(file4)) {
+			if (sb.getFile1().equals(file5) && sb.getFile2().equals(file4)) {
 				assertEquals(3, sb.getCount());
 
 			}
@@ -60,10 +61,10 @@ public class DocumentsProcessorTest {
 
 	@org.junit.Test
 	public void printSimilarities() {
-		Similarities s1 = new Similarities(file1,file2);
+		Similarities s1 = new Similarities(file1, file2);
 		s1.setCount(1);
 		treeset2.add(s1);
-		Similarities s2 = new Similarities(file3,file2);
+		Similarities s2 = new Similarities(file3, file2);
 		s1.setCount(3);
 		treeset2.add(s2);
 		processor.printSimilarities(treeset2, 1);
@@ -77,7 +78,7 @@ public class DocumentsProcessorTest {
 
 	@org.junit.Test
 	public void processAndStore() {
-		tuplelistTest = processor.processAndStore(dir,sequenceFile, 4);
+		tuplelistTest = processor.processAndStore(dir1, sequenceFile, 4);
 		int size = 0;
 		for (Tuple<String, Integer> tuple : tuplelistTest) {
 			if (tuple.getLeft().equals(file1)) {
