67bdb2aef9fd9a12284f93cbd5b1b063c75e1f15
Clara Zhang <85580892+zhihuizhang0625@users.noreply.github.com>
2022-02-02 20:48:19 -0500

    Update DocumentsProcessorTest.java

--- DocumentsProcessorTest.java
+++ DocumentsProcessorTest.java
@@ -1,58 +1,56 @@
-import static org.junit.Assert.assertEquals;
-import org.junit.Before;
-import org.junit.jupiter.api.Test;
+import static org.junit.Assert.*;
 
 import java.util.ArrayList;
-import java.util.Comparator;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.TreeSet;
 
-public class DocumentsProcessorTest {
+public class Test {
 	DocumentsProcessor processor = new DocumentsProcessor();
 	Map<String, List<String>> testmap = new HashMap<>();
 	List<String> testlist = new ArrayList<>();
 	List<Tuple<String, Integer>> tuplelistTest = new ArrayList<>();
 	TreeSet<Similarities> treeset2 = new TreeSet<>();
-
-	@Test
-	void ProcessDocuments() {
-	testlist.add("thisis");
-	testlist.add("isa");
-	testlist.add("atest");
-	testlist.add("testdocument");
-	testmap = processor.processDocuments("/autograder/submission/test files", 2);
-	assertEquals(testlist, testmap.get("/autograder/submission/file1.txt"));
-}
-	@org.junit.jupiter.api.Test
-	void teststoreNWordSequences1() {
-	testmap = processor.processDocuments("/autograder/submission/test files", 4);
-	tuplelistTest = processor.storeNWordSequences(testmap, "/autograder/submission/nwordFilePath.txt");
-	int size = 0;
-	for(Tuple<String, Integer> tuple: tuplelistTest) {
-		if(tuple.getLeft().equals("/autograder/submission/file1.txt")) {
-		 size = tuple.getRight();
-		}
-	}
-	assertEquals(28, size);
-}
 	
-	@org.junit.jupiter.api.Test
-	void computeSimilarities() {
-	testmap = processor.processDocuments("/autograder/submission/test files 2", 3);
-	tuplelistTest = processor.storeNWordSequences(testmap, "/autograder/submission/nwordFilePath.txt");	
-	treeset2 = processor.computeSimilarities("/autograder/submission/nwordFilePath.txt",tuplelistTest);
-	for(Similarities sb : treeset2)
-	{
-		
-		if(sb.getFile1().equals("/autograder/submission/file5.txt")&& sb.getFile2().equals("/autograder/submission/file4.txt")) {
-			assertEquals(3, sb.getCount());
+	@org.junit.Test
+	public void ProcessDocuments() {
+			testlist.add("thisis");
+			testlist.add("isa");
+			testlist.add("atest");
+			testlist.add("testdocument");
+			testmap = processor.processDocuments("/autograder/submission/test files", 2);
+			assertEquals(testlist, testmap.get("/autograder/submission/file1.txt"));
+		}
+	
+	@org.junit.Test
+	public void teststoreNWordSequences1() {
+		testmap = processor.processDocuments("/autograder/submission/test files", 4);
+		tuplelistTest = processor.storeNWordSequences(testmap, "/autograder/submission/nwordFilePath.txt");
+		int size = 0;
+		for(Tuple<String, Integer> tuple: tuplelistTest) {
+			if(tuple.getLeft().equals("/autograder/submission/file1.txt")) {
+			 size = tuple.getRight();
+			}
+		}
+		assertEquals(28, size);
+	}
+	
+	@org.junit.Test
+	public void computeSimilarities() {
+		testmap = processor.processDocuments("/autograder/submission/test files 2", 3);
+		tuplelistTest = processor.storeNWordSequences(testmap, "/autograder/submission/nwordFilePath.txt");	
+		treeset2 = processor.computeSimilarities("/autograder/submission/nwordFilePath.txt",tuplelistTest);
+		for(Similarities sb : treeset2)
+		{
 			
+			if(sb.getFile1().equals("/autograder/submission/file5.txt")&& sb.getFile2().equals("/autograder/submission/file4.txt")) {
+				assertEquals(3, sb.getCount());
+				
+			}
 		}
 	}
-}
-	@org.junit.jupiter.api.Test
+	@org.junit.Test
 	void printSimilarities() {
 		Similarities s1 = new Similarities("/autograder/submission/file1.txt", "/autograder/submission/file2.txt");
 		s1.setCount(1);
@@ -70,10 +68,7 @@ public class DocumentsProcessorTest {
         assertEquals(s2,arr[0]);
     
 	}
-	
-	
-	
-	@org.junit.jupiter.api.Test
+	@org.junit.Test
 	void processAndStore() {
 		tuplelistTest = processor.processAndStore("/autograder/submission/test files", "/autograder/submission/sequenceFile.txt", 4);
 		int size = 0;
