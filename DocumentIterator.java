d21ae17e0711b9cd655c6d24b77fd195e747d0e3
Clara Zhang <85580892+zhihuizhang0625@users.noreply.github.com>
2022-02-03 20:06:31 -0500

    Update DocumentIterator.java

--- DocumentIterator.java
+++ DocumentIterator.java
@@ -26,7 +26,82 @@ public class DocumentIterator implements Iterator<String> {
         }
     }
 
-
+//    @Override
+//    public String next() {
+//
+//        if (!hasNext()) {
+//            throw new NoSuchElementException();
+//        }
+//        String answer = "";
+//        try {
+//            answer = "";
+//            for (int i = 0; i < n; i++) {
+//                while (Character.isLetter(this.c)) {
+//                    if (Character.isUpperCase((char) this.c)) {
+//                        this.c = Character.toLowerCase((char) this.c);
+//                    }
+//                    answer += (char) this.c; 
+//                    this.c = this.r.read();
+//                }
+//                if (i == 0) {
+//                    this.r.mark(100);
+//                }
+//                skipNonLetters();
+//            }
+//            this.r.reset();
+//            this.c = this.r.read();
+//
+//        } catch (IOException e) {
+//            throw new NoSuchElementException();
+//        }
+//
+//        return answer;
+//
+//    }
+//
+//    @Override
+//    public boolean hasNext() {
+//        int temp = this.c;
+//        try {
+//            this.r.mark(100);
+//        } catch (Exception e) {
+//        }
+//        int counter = 0;
+//        while (this.c != -1) {
+//            while (Character.isLetter(this.c)) {
+//                try {
+//                    this.c = this.r.read();
+//                } catch (IOException e) {
+//                // TODO Auto-generated catch block
+//                    e.printStackTrace();
+//                }
+//            }
+//            skipNonLetters();
+//            counter++;
+//
+//            if (counter == this.n) {
+//                try {
+//                    this.r.reset();
+//                    this.c = temp;
+//                    return true;
+//                } catch (Exception e) {
+//                    e.printStackTrace();
+//                }
+//            }
+//
+//        }
+//        try {
+//            this.r.reset();
+//            this.c = temp;
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+//        return false;
+//    }
+//}
+    
+    
+    
     
     @Override
 	public boolean hasNext() {
@@ -46,7 +121,10 @@ public class DocumentIterator implements Iterator<String> {
 			while (numWords < this.n && hasNext()) {
 				tmpans = answer;
 				while (Character.isLetter(this.c)) {
-					answer = answer + (char) this.c;
+                  if (Character.isUpperCase((char) this.c)) {
+                      this.c = Character.toLowerCase((char) this.c);
+                  }
+                 answer = answer + (char) this.c;
 					this.c = this.r.read();
 				}
 
